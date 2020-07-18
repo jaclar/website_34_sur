58f34348f6ce310505c619658fa248d86defb026
Lars Jacob <lars.jacob@auth0.com>
2020-07-18 19:53:06 -0300

    add velero and nosotros to front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import { Link } from 'gatsby'
 import React from 'react'
 import Helmet from 'react-helmet'
-import pic01 from '../assets/images/pic01.jpg'
-import pic02 from '../assets/images/pic02.jpg'
+import picSamurai from '../assets/images/landing/samurai.jpg'
+import picNosotros from '../assets/images/landing/nosotros.jpg'
 import pic03 from '../assets/images/pic03.jpg'
 import pic04 from '../assets/images/pic04.jpg'
 import pic05 from '../assets/images/pic05.jpg'
@@ -17,8 +17,9 @@ class HomeIndex extends React.Component {
         <Helmet
           title="34 Sur"
           meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
+            { name: 'description', content: 'Una fuente de información sobre veleros, navegación y otras boludeces' },
+
+            { name: 'keywords', content: 'sailing, velero, sailboat, navegación, nautica' },
           ]}
         ></Helmet>
 
@@ -26,27 +27,27 @@ class HomeIndex extends React.Component {
 
         <div id="main">
           <section id="one" className="tiles">
-            <article style={{ backgroundImage: `url(${pic01})` }}>
+            <article style={{ backgroundImage: `url(${picSamurai})` }}>
               <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
+                aria-label="Vinculo a descripción del velero"
+                to="/velero"
                 className="link primary"
               >
                 <header className="major">
-                  <h3>Aliquam</h3>
-                  <p>Ipsum dolor sit amet</p>
+                <h3>Samurai</h3>
+                <p>El Velero - 侍</p>
                 </header>
               </Link>
             </article>
-            <article style={{ backgroundImage: `url(${pic02})` }}>
+            <article style={{ backgroundImage: `url(${picNosotros})` }}>
               <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
+                aria-label="Vinculo a la pagina sobre nosotros"
+                to="/nosotros"
                 className="link primary"
               >
                 <header className="major">
-                  <h3>Tempus</h3>
-                  <p>feugiat amet tempus</p>
+                  <h3>Nosotros</h3>
+                  <p>La tripulación</p>
                 </header>
               </Link>
             </article>

0f03107c13cc9c306db1fb442239c41751081351
Lars Jacob <lars.jacob@auth0.com>
2020-08-02 23:37:21 -0300

    cleanup index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,6 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import picSamurai from '../assets/images/landing/samurai.jpg'
 import picNosotros from '../assets/images/landing/nosotros.jpg'
-import pic03 from '../assets/images/pic03.jpg'
-import pic04 from '../assets/images/pic04.jpg'
-import pic05 from '../assets/images/pic05.jpg'
-import pic06 from '../assets/images/pic06.jpg'
 import Banner from '../components/Banner'
 import Layout from '../components/layout'
 
@@ -51,81 +47,6 @@ class HomeIndex extends React.Component {
                 </header>
               </Link>
             </article>
-            <article style={{ backgroundImage: `url(${pic03})` }}>
-              <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
-                className="link primary"
-              >
-                <header className="major">
-                  <h3>Magna</h3>
-                  <p>Lorem etiam nullam</p>
-                </header>
-              </Link>
-            </article>
-            <article style={{ backgroundImage: `url(${pic04})` }}>
-              <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
-                className="link primary"
-              >
-                <header className="major">
-                  <h3>Ipsum</h3>
-                  <p>Nisl sed aliquam</p>
-                </header>
-              </Link>
-            </article>
-            <article style={{ backgroundImage: `url(${pic05})` }}>
-              <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
-                className="link primary"
-              >
-                <header className="major">
-                  <h3>Consequat</h3>
-                  <p>Ipsum dolor sit amet</p>
-                </header>
-              </Link>
-            </article>
-            <article style={{ backgroundImage: `url(${pic06})` }}>
-              <Link
-                aria-label="Link to Landing Page"
-                to="/landing"
-                className="link primary"
-              >
-                <header className="major">
-                  <h3>Etiam</h3>
-                  <p>Feugiat amet tempus</p>
-                </header>
-              </Link>
-            </article>
-          </section>
-          <section id="two">
-            <div className="inner">
-              <header className="major">
-                <h2>Massa libero</h2>
-              </header>
-              <p>
-                Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-                Pellentesque condimentum sem. In efficitur ligula tate urna.
-                Maecenas laoreet massa vel lacinia pellentesque lorem ipsum
-                dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
-                sagittis libero. Mauris aliquet magna magna sed nunc rhoncus
-                amet pharetra et feugiat tempus.
-              </p>
-              <ul className="actions">
-                <li>
-                  <Link
-                    aria-label="Link to Landing Page"
-                    to="/landing"
-                    className="button next"
-                  >
-                    Get Started
-                  </Link>
-                </li>
-              </ul>
-            </div>
           </section>
         </div>
       </Layout>

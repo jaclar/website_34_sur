579b94f2cfa64f01b462eca7b81e40b9c4de92c0
Lars Jacob <lars.jacob@auth0.com>
2020-08-02 23:38:02 -0300

    new banner slogan

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,27 +1,26 @@
 import React from 'react'
 
 const Banner = props => (
-  <section id="banner" className="major">
-    <div className="inner">
-      <header className="major">
-        <h1>34° Sur</h1>
-      </header>
-      <div className="content">
-        <p>
-          Una pagina con informacion util sobre
-          <br />
-          navegacion y aspectos tecnicos de veleros.
-        </p>
-        <ul className="actions">
-          <li>
-            <a href="#one" name="Get Started" className="button next scrolly">
-              Ver Articulos
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </section>
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>34° Sur</h1>
+            </header>
+            <div className="content">
+                <p>
+                    es un sitio para documentar proyectos que estamos haciendo en el barco, <br />
+                    compartir cosas que aprendimos o que nos interesan.
+                </p>
+                <ul className="actions">
+                    <li>
+                        <a href="#one" name="Get Started" className="button next scrolly">
+                            Ver Articulos
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </section>
 )
 
 export default Banner

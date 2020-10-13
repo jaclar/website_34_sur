90682abb363c83c04da4cd994f8b10d6064af293
Lars Jacob <lars.jacob@auth0.com>
2020-10-13 19:13:15 -0300

    change inline image style

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,9 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1200,
+	       maxWidth: 600,
+	       showCaptions: true,
+               markdownCaptions: true
             },
           },
 	],
